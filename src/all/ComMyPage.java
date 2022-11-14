0df2c8b478b01e7a2426094f5fab801165da48e5
MoonNight285 <kimjyjh11@naver.com>
2022-11-14 11:56:28 +0900

    메인화면 UI 수정

--- src/all/ComMyPage.java
+++ src/all/ComMyPage.java
@@ -1,6 +1,7 @@
 package all;
 
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -15,6 +16,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
 
 public class ComMyPage {
 
@@ -59,6 +61,7 @@ public class ComMyPage {
       frame.setBounds(100, 100, Size.SCREEN_W, Size.SCREEN_H);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.getContentPane().setLayout(null);
+      frame.setTitle("다고쳐카센터 - 메인화면");
       
       JLabel lblNewLabel = new JLabel("New label");
       lblNewLabel.setIcon(new ImageIcon(ComMyPage.class.getResource("/img/YellowCat.png")));
@@ -71,18 +74,27 @@ public class ComMyPage {
       panel.setLayout(null);
       
       JButton btnEditComInfo = new JButton("업체정보 수정");
-      btnEditComInfo.setBounds(358, 41, 290, 45);
+      btnEditComInfo.setBounds(358, 51, Size.BTN_B_W, Size.BTN_B_H);
+      btnEditComInfo.setBackground(new Color(244, 204, 204));
+      btnEditComInfo.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnEditComInfo);
       btnEditComInfo.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       JButton btnEditTechList = new JButton("정비사 목록 수정");
-      btnEditTechList.setBounds(358, 127, 290, 45);
+      btnEditTechList.setBounds(358, 137, Size.BTN_B_W, Size.BTN_B_H);
+      btnEditTechList.setBackground(new Color(244, 204, 204));
+      btnEditTechList.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnEditTechList);
       btnEditTechList.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       
       JButton btnEditSrvInfo = new JButton("서비스 목록 수정");
-      btnEditSrvInfo.setBounds(358, 213, 290, 45);
+      btnEditSrvInfo.setBounds(358, 223, Size.BTN_B_W, Size.BTN_B_H);
+      btnEditSrvInfo.setBackground(new Color(244, 204, 204));
+      btnEditSrvInfo.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnEditSrvInfo);
       btnEditSrvInfo.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
@@ -91,27 +103,42 @@ public class ComMyPage {
       btnEditSrvInfo.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       JButton btnMgrUnitStock = new JButton("부품 재고 관리");
-      btnMgrUnitStock.setBounds(358, 299, 290, 45);
+      btnMgrUnitStock.setBounds(358, 309, Size.BTN_B_W, Size.BTN_B_H);
+      btnMgrUnitStock.setBackground(new Color(244, 204, 204));
+      btnMgrUnitStock.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnMgrUnitStock);
       btnMgrUnitStock.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       JButton btnMgrBook = new JButton("예약 관리");
-      btnMgrBook.setBounds(358, 385, 290, 45);
+      btnMgrBook.setBounds(358, 395, Size.BTN_B_W, Size.BTN_B_H);
+      btnMgrBook.setBackground(new Color(244, 204, 204));
+      btnMgrBook.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnMgrBook);
       btnMgrBook.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       JButton btnMgrCustomer = new JButton("고객 관리");
-      btnMgrCustomer.setBounds(358, 471, 290, 45);
+      btnMgrCustomer.setBounds(358, 481, Size.BTN_B_W, Size.BTN_B_H);
+      btnMgrCustomer.setBackground(new Color(244, 204, 204));
+      btnMgrCustomer.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnMgrCustomer);
       btnMgrCustomer.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       JButton btnMgrSales = new JButton("매출 관리");
-      btnMgrSales.setBounds(358, 557, 290, 45);
+      btnMgrSales.setBounds(358, 567, Size.BTN_B_W, Size.BTN_B_H);
+      btnMgrSales.setBackground(new Color(244, 204, 204));
+      btnMgrSales.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnMgrSales);
       btnMgrSales.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
       JButton btnMgrComment = new JButton("코멘트 관리");
-      btnMgrComment.setBounds(358, 643, 290, 45);
+      btnMgrComment.setBounds(358, 653, Size.BTN_B_W, Size.BTN_B_H);
+      btnMgrComment.setBackground(new Color(244, 204, 204));
+      btnMgrComment.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
       panel.add(btnMgrComment);
       btnMgrComment.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
       
