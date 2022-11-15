f226f5d0754f2722f8740f95320aeb7a8045a487
DESKTOP-6M69AE9\admin <nmk02@naver.com>
2022-11-15 11:41:38 +0900

    Mgr

--- src/all/UnitStockMgr.java
+++ src/all/UnitStockMgr.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
@@ -19,6 +20,7 @@ import javax.swing.JOptionPane;
 import java.awt.Component;
 import java.awt.ComponentOrientation;
 import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,6 +29,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Vector;
 
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
@@ -35,11 +39,17 @@ public class UnitStockMgr extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	private JTable tableUnitList;
 	private JButton btnDelUnitStock;
 	private JButton btnBackUnitStockMain;
 	private LoginManager loginManager;
 	private UnitStockMgr unitStockMgr;
-	
+	private JScrollPane scUnitList;
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/cardb?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul";
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
 
 //	private String header[] = {"stckNum", "부품번호","부품명","벤더", "재고수량"};
 //	private DefaultTableModel model = new DefaultTableModel(header, 0);
@@ -58,55 +68,70 @@ public class UnitStockMgr extends JFrame {
 			}
 		});
 	}
-
-
+	
 	
 	// Create the frame.
 	public UnitStockMgr() {
 		
+		setVisible(true);
+		setTitle("다고쳐카센터 - 부품재고관리");
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		
 		loginManager = loginManager.getInstance();
 		String id = loginManager.getLogComNum();
-		setVisible(true);
+		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, Size.SCREEN_W, Size.SCREEN_H);
 		contentPane = new JPanel();
 		contentPane.setEnabled(false);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
 		
-		// GwakMemberMgr에서 호출 -> 테이블 값 채워넣기
-		GwakMemberMgr mgr = new GwakMemberMgr();
-		GwakMemberBean bean = mgr.SelectUnitMgrTable(model, id);
-
+		DefaultTableModel model = new DefaultTableModel();
+		table = new JTable(model);
 		// 폼 창이 화면 가운데서 뜨게 하는 기능
 		setLocationRelativeTo(null); //--
 		setContentPane(contentPane);
 		
 		//테이블 생성
-		table = new JTable(model);
+		tableUnitList = new JTable(model);
 		
-		table.setFont(new Font("나눔바른고딕", Font.PLAIN, 21));
+		Vector<String> columnHeaders = new Vector<>();
+		columnHeaders.add("stckNum");
+		columnHeaders.add("부품번호");
+		columnHeaders.add("부품명");
+		columnHeaders.add("벤더");
+		columnHeaders.add("재고수량");
+		
+		HashMap<String, Integer> columnWidthValues = new HashMap<>();
+		columnWidthValues.put("stckNum", 20);
+		columnWidthValues.put("부품번호", 80);
+		columnWidthValues.put("부품명", 75);
+		columnWidthValues.put("벤더", 50);
+		columnWidthValues.put("재고수량", 50);
+		
+		TableDesigner.setFont(tableUnitList, "NanumBarunGothic", 21);
+		TableDesigner.setTableColumn(tableUnitList, columnHeaders);
+		TableDesigner.setTableTextCenter(tableUnitList, columnHeaders);
+		TableDesigner.resizeTableRow(tableUnitList, 50);
+		TableDesigner.resizeTableColumn(tableUnitList, columnWidthValues);
+		TableDesigner.resizeTableHeader(tableUnitList);
 
-		table.getColumnModel().getColumn(0).setPreferredWidth(39);
-		table.getColumnModel().getColumn(0).setMinWidth(20);
-		table.getColumnModel().getColumn(3).setResizable(false);
+		
+		scUnitList = new JScrollPane(tableUnitList);
+		scUnitList.setEnabled(false);
+		scUnitList.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scUnitList.setFont(new Font("나눔바른고딕", Font.PLAIN, 19));
+		scUnitList.setBounds(100, 145, 1462, 750);
 		contentPane.setLayout(null);
-
-		table.setBounds(247, 231, 1170, 671);
+		contentPane.add(tableUnitList);
 		
-		table.getColumn("stckNum").setWidth(0);
-		table.getColumn("stckNum").setMinWidth(0);
-		table.getColumn("stckNum").setMaxWidth(0);
-		
-//		테이블에 열 제목 나오게 하는 코드. 참고 : https://yyman.tistory.com/550
-		JScrollPane scrollPane = new JScrollPane(table); //
-		scrollPane.setFont(new Font("나눔바른고딕", Font.PLAIN, 21));
-		
-		scrollPane.setBounds(239, 236, 1186, 533);
-		scrollPane.setAutoscrolls(true);
-		contentPane.add (scrollPane) ;
-		
-//		테이블 행 높이 조절
-		table.setRowHeight(40);
+		JPanel jpList = new JPanel();		
+	    jpList.setLayout(new GridBagLayout());			
+		JScrollPane scrollSingle = new JScrollPane(jpList, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+		ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollSingle.setPreferredSize(new Dimension(400, 200));
 
 		// 품목 추가 버튼
 		JButton btnAddUnitStock = new JButton("품목 추가");
@@ -153,6 +178,8 @@ public class UnitStockMgr extends JFrame {
 		btnAddUnitStockHistory.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, Color.red, Color.red));
 		contentPane.add(btnAddUnitStockHistory);
 		
+		
+		getInfo();
 		
 		// 구매 이력 버튼 누르면 실행 됨 -> 새 폼 띄우기
 		btnUnitBuyHistory.addActionListener(new ActionListener() {
@@ -255,25 +282,66 @@ public class UnitStockMgr extends JFrame {
 		// 돌아가기 버튼 누르면 실행됨 -> 현재 화면 닫고 메인화면 띄우기
 		btnBackUnitStockMain.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// 메인화면은 visible true, 현재화면은 false
-
-			}
+						new ComMyPage();
+						dispose();
+				}		
 		});
 
 		unitStockMgr = this;
 	}
 	
+	
+	// GwakMemberMgr에서 호출 -> 테이블 값 채워넣기
+	//GwakMemberMgr mgr = new GwakMemberMgr();
+	//GwakMemberBean bean = mgr.SelectUnitMgrTable(tableUnitList, id);
+	
+	private void getInfo() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		loginManager = loginManager.getInstance();
+		String id = loginManager.getLogComNum();
+
+			try {
+				Class.forName(driver);
+				conn = DriverManager.getConnection(url,"root","1234");
+				sql = "SELECT stock.stckNum, stock.stckUnitNum, unit.unitName, unit.unitVendor, sum(stock.stckQty2) "
+						+ "FROM stock " 
+						+ "inner join unit "
+						+ "on stock.stckUnitNum = unit.unitNum "
+						+ "WHERE stock.stckComNum = ? "
+						+ "group by stock.stckUnitNum "
+						+ "ORDER BY stock.stckUnitNum ";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, id);
+				rs = pstmt.executeQuery();
+	
+				DefaultTableModel model = (DefaultTableModel)tableUnitList.getModel();
+				while(rs.next()) {
+					model.addRow(new Object[] {
+							rs.getInt("stock.stckNum"),
+							rs.getString("stock.stckUnitNum"),
+							rs.getString("unit.unitName"),
+							rs.getString("unit.unitVendor"),
+							rs.getString("sum(stock.stckQty2)"),
+					});
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+
+			}
+		}
+	
 	public void requestSelectUnit(UnitStockMgr unitStockMgr) {
 		if(unitStockMgr.equals(this)) {
-			loginManager = loginManager.getInstance();
-			String id = loginManager.getLogComNum();
-			GwakMemberMgr mgr = new GwakMemberMgr();
-			GwakMemberBean bean = mgr.SelectUnitMgrTable(model, id);
+			getInfo();
 
 		}
 	}
-	
-			}
+}
 	
 	
 
